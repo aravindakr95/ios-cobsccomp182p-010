a34cc86b28dd7ff45a1c074a964bde71ba9614e8
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-02-24 01:07:58 +0530

    Add biometrics blocked view controller and remove google plist

---
+++ nibm-events/controllers/BioMetricsBlockedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BioMetricsBlockedViewController.swift
+//  nibm-events
+//
+//  Created by Aravinda Rathnayake on 2/24/20.
+//  Copyright © 2020 Aravinda Rathnayake. All rights reserved.
+//
+
+import UIKit
+
+class BioMetricsBlockedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
