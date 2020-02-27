374735a5adf16f3b99bb9cbbb0cbfbe62efa4ff0
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-02-27 22:45:26 +0530

    Add navigation controller for signin process

---
+++ nibm-events/controllers/home/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  nibm-events
+//
+//  Created by Aravinda Rathnayake on 2/27/20.
+//  Copyright © 2020 Aravinda Rathnayake. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
