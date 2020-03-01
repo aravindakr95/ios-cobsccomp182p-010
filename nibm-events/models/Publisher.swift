b6c749320e22ee074fac5eba46fe6a5d9dfd9c72
Aravinda Rathnayake <aravindacloud@gmail.com>
2020-03-01 12:05:49 +0530

    F*cked up commit

---
+++ nibm-events/models/Publisher.swift
@@ -0,0 +1,9 @@
+//
+//  Publisher.swift
+//  nibm-events
+//
+//  Created by Aravinda Rathnayake on 3/1/20.
+//  Copyright © 2020 Aravinda Rathnayake. All rights reserved.
+//
+
+import Foundation
---
+++ nibm-events/models/UploadType.swift
@@ -0,0 +1,9 @@
+//
+//  UploadType.swift
+//  nibm-events
+//
+//  Created by Aravinda Rathnayake on 2/29/20.
+//  Copyright © 2020 Aravinda Rathnayake. All rights reserved.
+//
+
+import Foundation
